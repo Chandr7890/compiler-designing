b7e513fa825205c5ea670a78bb861e58de3cf289
chandra <113223913+Chandr7890@users.noreply.github.com>
2022-09-23 09:39:15 +0530

    no of macrons

---
+++ macron.c
@@ -0,0 +1,15 @@
+#define PI 3.14
+#define si 5.6
+#include<stdio.h>
+#include<conio.h>
+#include<string.h>
+#include<stdlib.h>
+void main()
+{
+ int a,b,c = 30;
+String name="chandra";
+
+
+ printf("hello");
+printf("hsk");
+ }
